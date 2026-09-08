9b5d54308736cbfa397d4c74532590e3c60336a5
jenilraiyani <jenil6958@gmail.com>
2026-09-08 20:00:48 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Should use environment variable in production
+    baseURL: 'https://task-managment-h2fy.vercel.app', // Should use environment variable in production
 });
 
 api.interceptors.request.use((config) => {
