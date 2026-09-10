3918a98d44d314948df20016a374a46ffe61e47c
jenilraiyani <jenil6958@gmail.com>
2026-09-10 18:01:59 +0530

    Change server listen address to '0.0.0.0'

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,7 +37,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
+ app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
   });
 });
