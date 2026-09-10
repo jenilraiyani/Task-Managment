4bb412a718f90bea0c57d0e409eee07d229ea762
jenilraiyani <jenil6958@gmail.com>
2026-09-10 18:16:08 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://task-managment-7b69.onrender.com', // Should use environment variable in production
+    baseURL: 'https://task-managment-7b69.onrender.com/api', // Should use environment variable in production
 });
 
 api.interceptors.request.use((config) => {
